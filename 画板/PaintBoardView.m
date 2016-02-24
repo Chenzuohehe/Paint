f8ee766cede45d7a2f289c36efb9e48c0c106a6b
Chenzuohehe <chenzuo0730@outlook.com>
2016-02-24 16:49:18 +0800

    1

---
+++ 画板/PaintBoardView.m
@@ -0,0 +1,21 @@
+//
+//  PaintBoardView.m
+//  画板
+//
+//  Created by MAC on 16/2/24.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import "PaintBoardView.h"
+
+@implementation PaintBoardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
