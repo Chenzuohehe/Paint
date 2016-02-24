859e0f630c7d27755b98865818bbdfd8e1881267
Chenzuohehe <chenzuo0730@outlook.com>
2016-02-24 17:01:41 +0800

    ce

--- 画板/ViewController.m
+++ 画板/ViewController.m
@@ -22,11 +22,6 @@
     paint.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:paint];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 40, self.view.frame.size.width, 40)];
-    label.text = @" 佐佐，先在画板上画红线，再使用橡皮";
-    label.font = [UIFont boldSystemFontOfSize:20];
-    [self.view addSubview:label];
-    
 }
 
 @end
